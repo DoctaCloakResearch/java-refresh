605017bc0e112927810ab2ea22eba6d526c992ef
Christopher Taylor <doctacloak@protonmail.com>
2018-10-02 16:16:15 -0500

    Completed char and boolean.

---
+++ CharAndBoolean/src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // Width of 16 ( 2 bytes )
+        char myChar = 'c';
+
+        boolean isHappy = true;
+        char specialCase = '\u00A9';
+
+        // Challenge
+        char myRegisteredSymbol = '\u00AE';
+        System.out.println("DoctaCloak" + myRegisteredSymbol);
+    }
+}

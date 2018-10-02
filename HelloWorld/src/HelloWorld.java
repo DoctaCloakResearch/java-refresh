e3df25b8efc39106e7548572a6bc5ddfe8a92979
Christopher Taylor <doctacloak@protonmail.com>
2018-10-02 15:26:56 -0500

    completed HelloWorld program.

---
+++ HelloWorld/src/HelloWorld.java
@@ -0,0 +1,15 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+
+        System.out.println("Hello, Christopher!");
+        int myFirstNumber = 10;
+        int mySecondNumber = 12;
+        int myThirdNumber = (myFirstNumber + mySecondNumber) + 50;
+        int myLastOne = 1000 - myThirdNumber;
+        System.out.println(myFirstNumber);
+        System.out.println(myLastOne);
+    }
+}
+
+
+

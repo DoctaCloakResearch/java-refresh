109eb03373912aed391f54da7df8fe380c236fbb
Christopher Taylor <doctacloak@protonmail.com>
2018-10-02 16:37:48 -0500

    completed strings challenge.

---
+++ Strings/src/Main.java
@@ -0,0 +1,26 @@
+public class Main {
+    public static void main(String[] args) {
+        /*
+        byte
+        short
+        int
+        long
+        float
+        double
+        char
+        boolean
+        string
+
+
+         */
+        String myString = "Hello friend,";
+        String anotherString = "This is another string.";
+        System.out.println(myString);
+        System.out.println(myString + " how are you?");
+
+        String lastString = "10";
+        int myInt = 50;
+        lastString = lastString + myInt;
+        System.out.println(lastString);
+    }
+}

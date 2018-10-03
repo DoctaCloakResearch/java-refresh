95c9a81682e9814310a340041af000f9e602032d
Christopher Taylor <doctacloak@protonmail.com>
2018-10-03 14:24:41 -0500

    completed encapsulation.

---
+++ Encapsulation/src/com/doctacloak/Main.java
@@ -0,0 +1,9 @@
+package com.doctacloak;
+
+public class Main {
+
+    public static void main(String[] args) {
+        EnhancedPlayer player = new EnhancedPlayer("Tim", 50, "Sword");
+        System.out.println("Initial health is " + player.getHealth());
+    }
+}
---
+++ Encapsulation/src/com/doctacloak/Player.java
@@ -0,0 +1,18 @@
+package com.doctacloak;
+
+public class Player {
+    public String name;
+    public int health;
+    public String weapon;
+
+    public void loseHealth(int damage) {
+        this.health = this.health - damage;
+        if(this.health <= 0) {
+            System.out.println("The player is now dead.");
+        }
+    }
+
+    public int healthRemaining() {
+        return this.health;
+    }
+}

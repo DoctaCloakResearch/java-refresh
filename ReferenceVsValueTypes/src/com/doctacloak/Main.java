e58e5374fa299b05f186217c872cbcac969eab02
Christopher Taylor <doctacloak@protonmail.com>
2018-10-03 18:15:52 -0500

    completed referencevsvaluetype

--- ReferenceVsValueTypes/src/com/doctacloak/Main.java
+++ ReferenceVsValueTypes/src/com/doctacloak/Main.java
@@ -1,9 +1,10 @@
 package com.doctacloak;
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main {
-
+    private static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
 
 	    int myIntValue = 10;
@@ -25,11 +26,44 @@ public class Main {
         System.out.println("anotherArary after change = " + Arrays.toString(anotherArray));
 
         modifyArray(myIntArray);
+        System.out.println("Enter count: ");
+        int count = scanner.nextInt();
+        scanner.nextLine();
+
+        int[] returnedArray = readIntegers(count);
+        int returnedMin = findMin(returnedArray);
+
+        System.out.println("min = " + returnedMin);
     }
 
     private static void modifyArray(int[] array) {
 
         array[0] = 2;
         array = new int[] { 1, 2, 3, 4, 5};
+    }
+
+    private static int[] readIntegers(int count) {
+        int[] array = new int[count];
+        for (int i = 0; i < array.length; i++) {
+            System.out.println("Enter a number: ");
+            int number = scanner.nextInt();
+            scanner.nextLine(); // avoid input error
+            array[i] = number;
+        }
+        return array;
+    }
+
+    private static int findMin(int[] array) {
+        int min = Integer.MAX_VALUE;
+
+        for(int i = 0; i < array.length; i++) {
+            int value = array[i];
+
+            if (value < min) {
+                min = value;
+            }
+        }
+
+        return min;
     }
 }
